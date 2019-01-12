7310f73fc9d4d802aec4b4c467a3416742f77387
Andrew Jaffie <ajaffie@gmail.com>
2019-01-12 15:53:16 -0500

    renamed defaults

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -13,8 +13,8 @@ import frc.robot.Robot;
 /**
  * An example command.  You can replace me with your own command.
  */
-public class ExampleCommand extends Command {
-  public ExampleCommand() {
+public class DriveCommand extends Command {
+  public DriveCommand() {
     // Use requires() here to declare subsystem dependencies
     requires(Robot.m_subsystem);
   }
